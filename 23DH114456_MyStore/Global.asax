136a608b6b0b84998295d58eaae52aacfd546b12
HOME <HOME@LAPTOP-QTES5C6E>
2024-10-29 08:24:59 +0700

    Add project files.

---
+++ 23DH114456_MyStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH114456_MyStore.MvcApplication" Language="C#" %>
---
+++ 23DH114456_MyStore/Models/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList.Mvc;
+
+namespace _23DH114456_MyStore.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        public string SearchTerm { get; set; }
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        //Thứ tự sắp xếp
+        public string SortOrder { get; set; }
+
+        //Thuộc tính hỗ trợ phân trang
+        public int PageNumber { get; set; }  //Trang hiện tại
+        public int PageSize { get; set; } = 10;  //Số sản phẩm mỗi trang
+
+        //Danh sách sản phẩm đã phân trang
+        public PagedList.IPagedList<Product> Products { get; set; }
+
+        //Danh sách sản phẩm thỏa điều kiện cần thiết
+        //public List<Product> Products { get; set; }
+    }
+}
